e0986ad023988f512aebc34e241156fd8f9efd7f
yksen <selegakamil@gmail.com>
2021-11-27 21:54:03 +0100

    Lista 4. Zadanie 2.

--- Lista_4/zad2_lista4.py
+++ Lista_4/zad2_lista4.py
@@ -1,2 +1,16 @@
 # Kamil Selega zadanie 2 lista 4
 
+def pierwiastekPiategoStopnia(x0):
+    tolerancja = 10**-9
+    poprzedni = x0
+    # Liczenie kolejnego przybliżenia pierwiastka metodą Newtona
+    aktualny = (1/5) * (4*x0 + (x0/(x0**4)))
+    # Liczenie kolejnych przybliżeń do momentu gdy różnica dwóch kolejnych jest mniejsza od zdefiniowanej
+    while abs(aktualny - poprzedni) > tolerancja:
+        poprzedni = aktualny
+        aktualny = (1/5) * (4*aktualny + (x0/(aktualny**4)))
+    return aktualny
+
+# Pierwiastek piątego stopnia pierwszysch 100 liczb naturalnych
+for i in range(1, 101):
+    print(i, "\t", pierwiastekPiategoStopnia(i), "\t", i**(1/5))
