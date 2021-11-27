6ea1b229dc3232a3f2e03a1e05f27fcb700e55dd
yksen <selegakamil@gmail.com>
2021-11-27 23:24:52 +0100

    Lista 4. Zadanie 7.

---
+++ Lista_4/zad7_lista4.py
@@ -0,0 +1,12 @@
+# Kamil Selega zadanie 7 lista 4
+
+import numpy as np
+
+# Deklaracja wielomianu podanego na liście
+w = np.poly([1, 5 + 1j, -(8 - 5j), (30 - 14j), -84])
+
+# Pierwiastki wielomianu
+x = np.roots(w);
+
+# Wyświetlenie pierwiastków
+print(x)
