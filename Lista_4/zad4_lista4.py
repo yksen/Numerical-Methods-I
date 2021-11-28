d6d38920a19b2da370baaf9bcf269e1ac09ea8ec
yksen <selegakamil@gmail.com>
2021-11-28 21:22:04 +0100

    Lista 4. Zadanie 4.

--- Lista_4/zad4_lista4.py
+++ Lista_4/zad4_lista4.py
@@ -3,31 +3,92 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Promień okręgu
-r = 2
+# Możliwość włączenia rysowania wykresu
+rysuj_wykres = False
 
-# Współrzędne n punktów oddalonych o promień r od punktu (0, 0)
-n = 1000
-theta = np.linspace(0, 2*np.pi, n)
-x = r * np.cos(theta)
-y = r * np.sin(theta)
+# Deklaracja pierwszego równania przekształconego dla y przy pomocy WolframAlpha
+def pierwszeRownanie(x):
+    x = np.complex128(x)
+    y = np.real((-1) ** (2/3) * np.power((-x - np.exp(-x)), 1/3))
+    return y
 
-# Naniesienie okręgu na wykres
-plt.plot(x, y)
+# Deklaracja drugiego równania przekształconego dla y przy pomocy WolframAlpha
+def drugieRownanie(x):
+    y1 = x - np.sqrt(2*x**2 + np.tan(x))
+    y2 = np.sqrt(2*x**2 + np.tan(x)) + x
+    return (y1, y2)
 
-# Pierwsza funkcja w dziedzinie okolicy okręgu
-x = np.linspace(-r - 0.5, r + 0.5, n, dtype=np.complex128)
-y = (-1) ** (2/3) * np.power((-x - np.exp(-x)), 1/3)
+if rysuj_wykres:
+    # Ilość punktów
+    n = 1000000
 
-# Naniesienie pierwszej funkcji na wykres
-plt.plot(x, y)
+    # Wykres okręgu
+    r = 2
+    theta = np.linspace(0, 2*np.pi, n)
+    x = r * np.cos(theta)
+    y = r * np.sin(theta)
+    plt.plot(x, y)
 
-# Druga funkcja
-y = x - np.sqrt(2*x**2 + np.tan(x))
-plt.plot(x, y)
-y = np.sqrt(2*x**2 + np.tan(x)) + x
-plt.plot(x, y)
+    # Wykres pierwszego równania
+    x = np.linspace(-r - 0.5, r + 0.5, n, dtype=np.complex128)
+    y = pierwszeRownanie(x)
+    plt.plot(x, y)
 
-# Wyrównanie osi i wyświetlenie okna
-plt.axis('equal')
-plt.show()
+    # Wykres drugiego równania
+    x = np.linspace(-r - 0.5, r + 0.5, n)
+    y = drugieRownanie(x)
+    plt.plot(x, y[0], "r")
+    plt.plot(x, y[1], "r")
+
+    # Wyświetlenie wykresu
+    plt.xlim([-2.5, 2.5])
+    plt.ylim([-2.5, 2.5])
+    plt.show()
+
+    # Na wykresie widoczne jest przecięcie tych dwóch równań w 3 miejscach wewnątrz okręgu
+    # w przedziale mniej więcej od -1,3 do 1,3.
+
+# Tolerancja i krok startowy (względnie duży)
+tolerancja = 10**-7
+startowy_krok = 10**-2
+krok = startowy_krok
+
+# Deklaracja pustej tablicy rozwiązań, początkowego x i pierwszych wartości y
+rozwiazania = []
+x = -1.3
+y1 = pierwszeRownanie(x)
+y2 = drugieRownanie(x)[0]
+nast_y1 = pierwszeRownanie(x + krok)
+nast_y2 = drugieRownanie(x + krok)[0]
+znaleziono = False
+
+# Pętla szukająca wszystkich (3) zaobserwowanych na wykresie rozwiązań 
+while len(rozwiazania) < 3:
+    # Jeśli te dwie funkcje się przecinają pomiędzy kolejnymi dużymi krokami, szukaj rozwiązania
+    if (abs(y1) - abs(y2)) * (abs(nast_y1) - abs(nast_y2)) < 0:
+        # Dopóki nie znaleziono rozwiązania zmniejszam krok o 0.1 co przeszukanie przedziału
+        while not znaleziono:
+            a = x
+            # Przeszukiwanie przedziału z zadanym krokiem
+            while a < x + startowy_krok:
+                y1 = pierwszeRownanie(a)
+                y2 = drugieRownanie(a)[0]
+                # Jeśli różnica wartości y obydwu równań jest mniejsza od ustalonej tolerancji znaleziono rozwiązanie
+                if abs(abs(y1) - abs(y2)) < tolerancja:
+                    rozwiazania.append((a, y1))
+                    znaleziono = True                    
+                    break
+                a += krok
+            krok *= 10**-1
+    # Zresetowanie wartości do przeszukiwania znalezionego przedziału
+    znaleziono = False
+    krok = startowy_krok
+    # Aktualizacja wartości do poszukiwania odpowiedniego kandydata na przedział zawierający rozwiązanie
+    x += krok
+    y1 = pierwszeRownanie(x)
+    y2 = drugieRownanie(x)[0]
+    nast_y1 = pierwszeRownanie(x + krok)
+    nast_y2 = drugieRownanie(x + krok)[0]
+
+# Wyświetlenie rozwiązań
+print(rozwiazania)
