97835cac6d86656303a470444964725f68d5ba3f
yksen <selegakamil@gmail.com>
2021-11-30 12:43:03 +0100

    Lista 5. Zadanie 1.

--- Lista_5/zad1_lista5.py
+++ Lista_5/zad1_lista5.py
@@ -8,7 +8,7 @@ h = np.array([0, 3, 6])
 rho = np.array([1.225, 0.905, 0.652])
 plt.plot(h, rho)
 
-# Dopasowanie wielomianu stopnia drugiego do danych
+# Dopasowanie współczynników wielomianu stopnia drugiego do danych
 a = np.polyfit(h, rho, 2)
 x = np.linspace(h[0] - 1, h[-1] + 1, 100)
 y = a[0] * x**2 + a[1] * x**1 + a[2] * x**0
