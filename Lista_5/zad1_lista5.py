c2b9cada475aa92861797feec4d55e12ab27fa1c
yksen <selegakamil@gmail.com>
2021-11-30 12:33:23 +0100

    Lista 5. Zadanie 1.

---
+++ Lista_5/zad1_lista5.py
@@ -0,0 +1,17 @@
+# Kamil Selega zadanie 1 lista 5
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Dane
+h = np.array([0, 3, 6])
+rho = np.array([1.225, 0.905, 0.652])
+plt.plot(h, rho)
+
+# Dopasowanie wielomianu stopnia drugiego do danych
+a = np.polyfit(h, rho, 2)
+x = np.linspace(h[0] - 1, h[-1] + 1, 100)
+y = a[0] * x**2 + a[1] * x**1 + a[2] * x**0
+plt.plot(x, y)
+
+plt.show()
