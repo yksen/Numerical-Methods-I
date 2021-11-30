5659161074a00f64b81f75028bc7533e1767a0d4
yksen <selegakamil@gmail.com>
2021-11-30 13:43:53 +0100

    Lista 5. Zadanie 2.

---
+++ Lista_5/zad2_lista5.py
@@ -0,0 +1,13 @@
+# Kamil Selega zadanie 2 lista 5
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Dane
+x = np.linspace(1, 3, 9)
+y = np.array([-0.5403, -0.0104, 0.9423, 1.7445, 1.3073, -0.7718, -2.4986, -0.7903, 2.7334])
+plt.plot(x, y)
+
+
+
+plt.show()
