ec4788d5f0eabec07a7497a73aef27047ab1fcbf
yksen <selegakamil@gmail.com>
2021-12-07 14:19:13 +0100

    Lista 5. Zadanie 2.

--- Lista_5/zad2_lista5.py
+++ Lista_5/zad2_lista5.py
@@ -4,10 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Dane
-x = np.linspace(1, 3, 9)
+x = np.arange(1, 3.25, 0.25)
 y = np.array([-0.5403, -0.0104, 0.9423, 1.7445, 1.3073, -0.7718, -2.4986, -0.7903, 2.7334])
+
 plt.plot(x, y)
-
-
-
 plt.show()
