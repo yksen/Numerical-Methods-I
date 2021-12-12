a5bb5ae0565f7d56f1ca39c03c80c10f81dc517b
yksen <selegakamil@gmail.com>
2021-12-12 16:08:45 +0100

    Lista 5. Zadanie 2.

--- Lista_5/zad2_lista5.py
+++ Lista_5/zad2_lista5.py
@@ -1,11 +1,17 @@
 # Kamil Selega zadanie 2 lista 5
 
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.interpolate import CubicSpline
 
 # Dane
 x = np.arange(1, 3.25, 0.25)
 y = np.array([-0.5403, -0.0104, 0.9423, 1.7445, 1.3073, -0.7718, -2.4986, -0.7903, 2.7334])
 
-plt.plot(x, y)
-plt.show()
+# Interpolacja funkcjami sklejanymi
+cs = CubicSpline(x, y)
+
+# Wartość y'(2.1)
+print("y'(2.1) = ", cs(2.1, 1))
+
+# Pierwiastki y(x)
+print(cs.roots())
