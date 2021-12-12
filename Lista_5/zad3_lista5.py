bae8ba1d9ce7345738846a83a3ad6e64258e3b89
yksen <selegakamil@gmail.com>
2021-12-12 15:26:47 +0100

    Lista 5. Zadanie 3.

--- Lista_5/zad3_lista5.py
+++ Lista_5/zad3_lista5.py
@@ -4,18 +4,18 @@ import numpy as np
 from scipy.interpolate import CubicSpline
 import matplotlib.pyplot as plt
 
-# Dane
-Re = np.log10(np.array([0.2, 2, 20, 200, 2000, 20000]))
-cp = np.log10(np.array([103, 13.9, 2.72, 0.8, 0.401, 0.433]))
+# Dane w skali podwójnie logarytmicznej
+Re = np.array([0.2, 2, 20, 200, 2000, 20000])
+cp = np.array([103, 13.9, 2.72, 0.8, 0.401, 0.433])
 
 # Interpolacja
 xs = np.arange(Re[0], Re[-1], 0.01)
-cs = CubicSpline(Re, cp)
+cs = CubicSpline(np.log10(Re), np.log10(cp))
 
-# Wartości dla Re = 5.5 i Re = 5000
-print("Re = 5.5, cp = ", cs(np.log10(5.5)), "\nRe = 5000, cp = ", cs(np.log10(5000)))
+# Wartości dla Re = 5, Re = 50 i Re = 5000
+print("Re = 5, \tcp =", 10**cs(np.log10(5)),"\nRe = 50,\tcp =", 10**cs(np.log10(50)) , "\nRe = 5000,\tcp =", 10**cs(np.log10(5000)))
 
 # Wykres
 plt.scatter(Re, cp)
-plt.plot(xs, cs(xs))
+plt.loglog(xs, 10**cs(np.log10(xs)))
 plt.show()
