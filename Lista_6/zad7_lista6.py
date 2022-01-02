8bb113ac4d95558d33b07210164ec29b598d6e14
yksen <selegakamil@gmail.com>
2022-01-02 18:48:32 +0100

    Lista 6. Zadanie 7.

--- Lista_6/zad7_lista6.py
+++ Lista_6/zad7_lista6.py
@@ -14,5 +14,8 @@ print(quadrature(f, -1, 1))
 # Całki na dwóch osobnych przedziałach po lewej i prawej stronie od punktu x=0
 sum1 = quadrature(f, -1, 0)
 sum2 = quadrature(f, 0, 1)
-# Suma wartości tych całek i gorsza z ich dwóch dokładności która i tak jest rzędu e-10
-print((sum1[0] + sum2[0], max([sum1[1], sum2[1]])))
+# Suma wartości tych całek i suma ich dwóch dokładności 
+print((sum1[0] + sum2[0], sum1[1] + sum2[1]))
+
+# Zauważyłem, że oba te wyniki zaczynają się rozbiegać dopiero na jedenastym miejscu
+# po przecinku co najprawdopodobniej świadczy o dokładności do dziesiątego miejsca.
