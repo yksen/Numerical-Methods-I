12b7164a5d9f7cd8ca59debb1488ad67ffc06974
yksen <selegakamil@gmail.com>
2022-01-02 18:37:16 +0100

    Lista 6. Zadanie 7.

---
+++ Lista_6/zad7_lista6.py
@@ -0,0 +1,18 @@
+# Kamil Selega zadanie 7 lista 6
+
+import numpy as np
+from scipy.integrate import quadrature
+
+# Deklaracja równania
+def f(x):
+    return (np.cos(x) - np.exp(x)) / np.sin(x)
+
+# Całka tego równania na całym przedziale.
+# Niestety funkcja na tym przedziale nie zwraca z jakiegość powodu dokładności.
+print(quadrature(f, -1, 1))
+
+# Całki na dwóch osobnych przedziałach po lewej i prawej stronie od punktu x=0
+sum1 = quadrature(f, -1, 0)
+sum2 = quadrature(f, 0, 1)
+# Suma wartości tych całek i gorsza z ich dwóch dokładności która i tak jest rzędu e-10
+print((sum1[0] + sum2[0], max([sum1[1], sum2[1]])))
