55a50c0bf937469a457040459ed0f1fcbaee6bf2
yksen <selegakamil@gmail.com>
2022-01-02 18:04:03 +0100

    Lista 6. Zadanie 8.

---
+++ Lista_6/zad8_lista6.py
@@ -0,0 +1,11 @@
+# Kamil Selega zadanie 8 lista 6
+
+import numpy as np
+from scipy.integrate import dblquad
+
+# Deklaracja równania
+def f(x, y):
+    return np.sin(np.pi * x) * np.sin(np.pi * (y - x))
+
+# Podwójna całka na zadanych przedziałach x: 0..1, y: 0..x
+print(dblquad(f, 0, 1, lambda x: 0, lambda x: x))
