fa7dce181cfcc781b4f641eeec418fa680488bcd
yksen <selegakamil@gmail.com>
2022-01-11 13:05:08 +0100

    Lista 7. Zadanie 1.

--- Lista_7/zad1_lista7.py
+++ Lista_7/zad1_lista7.py
@@ -10,8 +10,8 @@ def f(t, y):
 
 # Dane
 y0 = [2, 2.5, 3, 3.5]
-# Rozwiązanie w zadanycm przedziale
-rozwiazanie = solve_ivp(f, (0, 6), y0)
+# Rozwiązania w zadanycm przedziale
+rozwiazanie = solve_ivp(f, (0, 6), y0, t_eval=np.linspace(0, 6, 1000))
 
 # Wykres rozwiązań
 for i in range(len(y0)):
