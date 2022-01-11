53c389508dbc885d3fcf3ad004df019e98b4e90f
yksen <selegakamil@gmail.com>
2022-01-11 12:42:04 +0100

    Lista 7. Zadanie 1.

---
+++ Lista_7/zad1_lista7.py
@@ -0,0 +1,20 @@
+# Kamil Selega zadanie 1 lista 7
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+# Deklaracja równania
+def f(t, y):
+    return np.sin(t*y)
+
+# Dane
+y0 = [2, 2.5, 3, 3.5]
+# Rozwiązanie w zadanycm przedziale
+rozwiazanie = solve_ivp(f, (0, 6), y0)
+
+# Wykres rozwiązań
+for i in range(len(y0)):
+    plt.plot(rozwiazanie.t, rozwiazanie.y[i], label=y0[i])
+plt.legend()
+plt.show()
