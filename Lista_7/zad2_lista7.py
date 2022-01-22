67e385afad5c31b5e029890a39ca12d8cc1360b6
yksen <selegakamil@gmail.com>
2022-01-22 17:32:10 +0100

    Lista 7. Zadanie 2.

--- Lista_7/zad2_lista7.py
+++ Lista_7/zad2_lista7.py
@@ -17,7 +17,7 @@ def f(t, y, a):
     return [dtheta, a*np.cos(t*omega_sr) - np.sin(theta) - 1/Q*dtheta]
 
 # Deklaracja okna wykresu i jego podwykresów
-okno, osie = plt.subplots(2, 3)
+okno, wykr = plt.subplots(2, 3)
 
 # Przedział czasu
 t = np.linspace(0, 50, 1000)
@@ -25,12 +25,12 @@ t = np.linspace(0, 50, 1000)
 # Rozwiązanie dla każdego zestawu warunków początkowych i naniesienie ich na wykres wraz z opisem osi
 for i in range(len(theta_0)):
     rozw = solve_ivp(f, y0=[theta_0[i], dtheta_0], t_span=(t[0], t[-1]), t_eval=t, args=[A[i]], method="LSODA")
-    osie[0, i].plot(rozw.t, rozw.y[1])
-    osie[0, i].set_xlabel("t")
-    osie[0, i].set_ylabel("theta")
-    osie[1, i].plot(rozw.y[1], rozw.y[0])
-    osie[1, i].set_xlabel("theta")
-    osie[1, i].set_ylabel("dtheta")
+    wykr[0, i].plot(rozw.t, rozw.y[1])
+    wykr[0, i].set_xlabel("t")
+    wykr[0, i].set_ylabel("theta")
+    wykr[1, i].plot(rozw.y[1], rozw.y[0])
+    wykr[1, i].set_xlabel("theta")
+    wykr[1, i].set_ylabel("dtheta")
 
 # Wyświetlenie wykresu
 plt.show()
