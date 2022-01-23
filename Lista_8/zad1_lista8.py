2fa1fed6b945b8dd3f3de4527e65c7854b673ba0
yksen <selegakamil@gmail.com>
2022-01-23 15:58:20 +0100

    Lista 8. Zadanie 1.

--- Lista_8/zad1_lista8.py
+++ Lista_8/zad1_lista8.py
@@ -1,5 +1,6 @@
 # Kamil Selega zadanie 1 lista 8
 
+from contextlib import suppress
 import numpy as np
 from scipy import linalg
 
@@ -9,8 +10,8 @@ a = np.array([[-1, -4, 1], [-1, -2, -5], [5, 4, 3]])
 # Wartości i wektory własne
 eigen = linalg.eig(a)
 
+# Wyświetlenie sformatowanych wyników. 
+# Warto dodać, że wektory własne odczytywane są w pionie, a nie w poziomie.
+np.set_printoptions(3, suppress=True)
 print(f"Wartości własne:\n{eigen[0]}")
-
-print(f"Wektory własne:")
-for i in eigen[1]:
-    print([f"{j:.3f}" for j in i])
+print(f"Wektory własne:\n{eigen[1]}")
