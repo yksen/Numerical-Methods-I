562311ba066d9dddbf1cc054b2394ae3296b1aac
yksen <selegakamil@gmail.com>
2022-01-23 16:03:15 +0100

    Lista 8. Zadanie 2.

---
+++ Lista_8/zad2_lista8.py
@@ -0,0 +1,14 @@
+# Kamil Selega zadanie 2 lista 8
+
+import numpy as np
+from scipy import linalg
+
+# Dana macierz
+a = linalg.hilbert(6)
+
+# Wartości i wektory własne
+eigen = linalg.eig(a)
+
+# Wyświetlenie wyników
+print(f"Wartości własne:\n{eigen[0]}")
+print(f"Wektory własne:\n{eigen[1]}")
