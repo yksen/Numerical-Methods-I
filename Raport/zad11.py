955558a8ec89f49141e3adf73b557e52e9130114
yksen <selegakamil@gmail.com>
2022-01-31 13:46:47 +0100

    Raport, Zadanie 11.

---
+++ Raport/zad11.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+# Definicja równania różniczkowego
+def f(x, y):
+    return 2*x - y**2 - 1
+
+# Definicja funkcji znajdującej pierwiastki
+def pierwiastek(x, y):
+    return y
+
+# Dane
+x = (0, 4)
+y0 = [.2]
+
+# Rozwiązanie
+roz = solve_ivp(f, x, y0, events=pierwiastek, atol=1e-10, rtol=1e-12)
+
+# Znalezione pierwiastki
+print(f"Pierwiastki znalezione w badanym przedziale to: {roz.t_events[0]}")
+
+# Wykres rozwiązań
+plt.axhline(0, color="red", linestyle="--")
+plt.plot(roz.t, roz.y[0], label = "y(x)")
+plt.legend()
+plt.show()
